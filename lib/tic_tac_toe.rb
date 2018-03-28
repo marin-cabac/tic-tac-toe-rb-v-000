03f77ca8332d09afdf8566597a37beaa1e48708f
Marin Cabac <marin.cabac@gmail.com>
2018-03-28 01:21:57 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -17,20 +17,27 @@ def display_board(board)
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
+<<<<<<< HEAD
 
 #################################################
 
+=======
+>>>>>>> ec75a99cb341dac82d426ee23ce07d3d11788d0d
 def input_to_index(inp)
   inp.to_i-1
 end
 
+<<<<<<< HEAD
 #################################################
 
+=======
+>>>>>>> ec75a99cb341dac82d426ee23ce07d3d11788d0d
 def move(board,po,ch)
 board[po]=ch
 display_board(board)
 end
 
+<<<<<<< HEAD
 
 
 #################################################
@@ -56,32 +63,66 @@ end
 
 #################################################
 
+=======
+def position_taken?(board,idx)
+  board[idx]=="X"||board[idx]=="O"
+end
+
+def valid_move?(board,idx)
+  idx.between?(0,8) && !(position_taken?(board,idx))
+end
+
+def turn(board)
+  puts "Please choose btwn 1-9"
+  inp=input_to_index(gets.strip)
+  !(valid_move?(board,inp)) ? turn(board) : move(board,inp, value="X")
+  display_board(board)
+end
+
+>>>>>>> ec75a99cb341dac82d426ee23ce07d3d11788d0d
 def turn_count(board)
    board.count("X")+board.count("O")
 end
 
 def current_player(board)
+<<<<<<< HEAD
   turn_count(board).even? ? ("X") : ("O")
+=======
+  turn_count(board).even? ? "X" : "O"
+>>>>>>> ec75a99cb341dac82d426ee23ce07d3d11788d0d
 end
 
 ####################################################
 
 def won?(board)
+<<<<<<< HEAD
   WIN_COMBINATIONS.find do |idx|
-    if idx.all? {|i| board[i] == "X" } == true
+    if idx.all? {|i| board[i] == "X" }  
       idx
     elsif idx.all? {|i| board[i] == "O" } == true
       idx
+=======
+  board.all? {|i| i != " " || i != ""}
+  w_arr = WIN_COMBINATIONS.detect do |w_arr|
+    if w_arr.all? {|po| board[po] == "X" } == true
+      w_arr.inspect
+    elsif w_arr.all? {|po| board[po] == "O" } == true
+      w_arr.inspect
+>>>>>>> ec75a99cb341dac82d426ee23ce07d3d11788d0d
     end
   end
 end
 
+<<<<<<< HEAD
 #################################################
 
+=======
+>>>>>>> ec75a99cb341dac82d426ee23ce07d3d11788d0d
 def full?(board)
 board.all? {|i| i == "X" || i == "O" }
 end
 
+<<<<<<< HEAD
 ###################################################
 
 def draw?(board)
@@ -89,11 +130,18 @@ def draw?(board)
 end
 
 ###################################################
+=======
+def draw?(board)
+  won?(board)==nil&&full?(board)==true
+
+end
+>>>>>>> ec75a99cb341dac82d426ee23ce07d3d11788d0d
 
 def over?(board)
   draw?(board)||won?(board)
 end
 
+<<<<<<< HEAD
 ###################################################
 
 def winner(board)
@@ -109,4 +157,17 @@ until over?(board)
     turn(board)
   end
 draw?(board) ? (puts "Cat's Game!") :  (puts "Congratulations #{winner(board)}!" )
+=======
+def winner(board)
+  if  won?(board)!=nil
+     board[won?(board)[0]]
+  end
+end
+
+def play(board)
+inp=gets.strip
+until over?(board) == true
+    turn(board)
+  end
+>>>>>>> ec75a99cb341dac82d426ee23ce07d3d11788d0d
 end
